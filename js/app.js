04be922c39bb58a894ffe852f53db6dfd014225b
TamilWebCast <tamilwebcast.com@gmail.com>
2026-01-02 17:26:16 +0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -150,7 +150,7 @@ function playerSpinner(visible, msg) {
             `<span class="spinner"><svg class="animate-spin h-8 w-8 text-blue-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
                 <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
                 <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-            </svg></span> ${msg || "Loading Stream..."}`, 0
+            </svg>https://twciptv.github.io/TWC-IPTV.m3u</span> ${msg || "Loading Stream..."}`, 0
         );
     } else {
         hidePlayerNotification();
@@ -906,5 +906,6 @@ document.addEventListener('keydown', (event) => {
         showPlayerNotification(`Volume: ${Math.round(newVolume * 100)}%`, 1500);
     }
 });
+
 
 
